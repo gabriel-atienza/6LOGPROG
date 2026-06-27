f9e0251a8f6f7f1244d45032076aa7e5394b9a1a
Gabriel Atienza <gabrielatienza.dev@gmail.com>
2026-06-27 22:32:45 +0800

    prelim laboratory exercise 1

---
+++ LABS/PRELIM/activity1-atienza-gabriel.py
@@ -0,0 +1,25 @@
+# Prelim Laboratory Exercise 1
+# Atienza, Gabriel Anthony H.
+# 2258-6LOGPROG
+
+#variables
+name = "Maria" #name of the student
+age = 18 #age of the student
+numone = 15 #first number
+numtwo = 5 #second number
+sum = numone + numtwo #sum of numone and numtwo
+difference = numone - numtwo #difference of numone and numtwo
+product = numone * numtwo #product of numone and numtwo
+quotient = numone / numtwo #quotient of numone and numtwo
+
+#Student Information
+print("Student Name:", name) #prints the student name
+print("Age:", age) #prints the age of the student
+
+#Arithmetic Operations
+print("") #prints a blank space
+print("Arithmetic Results") #prints the string Arithmetic Results
+print("Addition:", sum) #prints the sum of numone and numtwo
+print("Subtraction:", difference) #prints the difference of numone and numtwo
+print("Multiplication:", product) #prints the product of numone and numtwo
+print("Division:", quotient) #prints the quotient of numone and numtwo
