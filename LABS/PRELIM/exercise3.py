80e3b22449f006c470fb9c96a1d18de2853a96f6
Gabriel Atienza <gabrielatienza.dev@gmail.com>
2026-07-03 17:37:54 +0800

    exercise 3

---
+++ LABS/PRELIM/exercise3.py
@@ -0,0 +1,12 @@
+#6LOGPROG Exercise 3
+#Atienza, Gabriel Anthony H.
+
+age = int(input("Enter your age: ")) #asks user for their age
+name = input("Enter your name: ") #asks user for their name
+address = input("Enter your address: ") #asks user for their address
+
+#print(type(age))
+print("User's name is: ", name) #prints the user's name
+print("User's age is: ", age) #prints the user's age
+print("User's address is: ", address) #prints the user's address
+#print("The user's name is " + name + " their age is " + str(age) + " year(s) old " + "and their address is " + address )
