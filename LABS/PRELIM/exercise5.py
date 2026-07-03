3d7ccf6541ef6e3a329edd834541c66ce8a2b133
Gabriel Atienza <gabrielatienza.dev@gmail.com>
2026-07-03 17:40:02 +0800

    exercise 5

---
+++ LABS/PRELIM/exercise5.py
@@ -0,0 +1,10 @@
+#6LOGPROG Exercise 5
+#Atienza, Gabriel Anthony H.
+
+#variables
+age = int(input("Input your age: ")) #asks for the user's age
+
+if age >= 18:   #checks if user's age is 18 or above
+    print("user is an adult.") #prints message if user is 18 or over
+else:         #else if user's age is below 18
+    print("user is underaged.") #prints message that user is underaged
