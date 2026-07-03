b7f522e293c85686842696877359be3a1b51fc5b
Gabriel Atienza <gabrielatienza.dev@gmail.com>
2026-07-03 17:38:42 +0800

    exercise 4

---
+++ LABS/PRELIM/exercise4.py
@@ -0,0 +1,10 @@
+#6LOGPROG Exercise 4
+#Atienza, Gabriel Anthony H.
+
+#variables
+grade = int(input("Input your grade: ")) #asks the user for their grade
+
+if grade >= 75 or grade <= 80: #checks if grade is passing 
+    print("User passed!")
+else:
+    print("User failed.")
